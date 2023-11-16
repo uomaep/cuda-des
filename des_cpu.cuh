1deb8bf073eea74a37722562df7457288631b456
uomaep <uomaep@gmail.com>
2023-11-16 15:07:00 +0900

    initial commit

---
+++ des_cpu.cuh
@@ -0,0 +1,115 @@
+#ifndef DES_CPU
+#define DES_CPU
+
+#include <stdlib.h>
+#include <time.h>
+#include <strings.h>
+#include <math.h>
+
+#include "des_constants.h"
+#include "des_gpu.cu"
+
+typedef unsigned long uint32;
+typedef unsigned long long uint64;
+
+__host__ uint64 generate_key(int key_size, uint64 key_range_mask);
+
+__host__ void generate_subkeys(uint64 key, uint64 * subkeys);
+
+__host__ uint64 encrypt_message(uint64 message, uint64 key);
+
+__host__ uint32 f(uint32 R, uint64 K);
+
+__host__ unsigned char get_S_value(unsigned char B, int s_idx);
+
+// ----------------------------------------
+
+__host__ uint64 generate_key(int key_size, uint64 key_range_mask) {
+	
+	uint64 key = 0;
+	for(int i=0; i<key_size; i++) {
+		key |= ((uint64)(rand() & 1) << i);
+	}
+	return key & key_range_mask;
+}
+
+__host__ void generate_subkeys(uint64 key, uint64 * subkeys) {
+	int size_PC1 = sizeof(PC_1)/sizeof(PC_1[0]);	
+	int size_PC2 = sizeof(PC_2)/sizeof(PC_2[0]);	
+
+	uint64 permuted_key = permute(key, PC_1, size_PC1);
+
+	uint32 C[17], D[17];
+
+	C[0] = (uint32) (permuted_key >> 28 & 0xFFFFFFF);
+	D[0] = (uint32) (permuted_key >> 0 & 0xFFFFFFF);
+
+	// apply left shifts
+	for(int i=1; i<=16; i++) {
+		C[i] = C[i-1] << SHIFTS[i-1];
+		D[i] = D[i-1] << SHIFTS[i-1];
+
+		C[i] |= C[i] >> 28;
+		D[i] |= D[i] >> 28;
+
+		C[i] &= ~(3UL << 28);
+		D[i] &= ~(3UL << 28);
+
+		uint64 merged_subkey = ((uint64)C[i] << 28) | D[i];
+		subkeys[i-1] = permute(merged_subkey, PC_2, size_PC2);
+	}
+}
+
+__host__ uint64 encrypt_message(uint64 message, uint64 key) {
+	uint64 K[16];
+	uint32 L[17], R[17];
+
+	generate_subkeys(key, K); // 16개의 라운드 키 생성
+
+	int size_IP = sizeof(IP)/sizeof(IP[0]);
+	uint64 IP_message = permute(message, IP, size_IP);
+
+	L[0] = (uint32) (IP_message >> 32 & 0xFFFFFFFF);
+	R[0] = (uint32) (IP_message >> 0 & 0xFFFFFFFF);
+
+	for(int i=1; i<=16; i++) {
+		L[i] = R[i-1];
+		R[i] = L[i-1] ^ f(R[i-1], K[i-1]);
+	}
+
+	uint64 RL = ((uint64) R[16] << 32) | L[16];
+	uint64 encrypted_message = permute(RL, IP_REV, 64);
+
+	return encrypted_message;
+
+}
+
+__host__ uint32 f(uint32 R, uint64 K) {
+	int size_E = sizeof(E_BIT)/sizeof(E_BIT[0]);
+	unsigned char S[8];
+	uint32 s_string = 0;
+	uint64 expanded_R = permute(R, E_BIT, size_E);
+
+	uint64 R_xor_K = expanded_R ^ K;
+
+	for(int i=0; i<8; i++) {
+		S[i] = get_S_value((unsigned char) (R_xor_K >> 6*(7 - i)) & 0x3F, i);
+		s_string |= S[i];
+		s_string <<= (i != 7) ? 4 : 0;
+	}
+	return (uint32) permute(s_string, P, 32);
+}
+
+__host__ unsigned char get_S_value(unsigned char B, int s_idx) {
+	unsigned int i = (((B >> 5) & 1U) << 1) | ((B >> 0) & 1U);
+	unsigned int j = 0;
+
+	for(int k=4; k>0; k--) {
+		j |= ((B >> k) & 1U);
+		j <<= (k != 1) ? 1 : 0;
+	}
+
+	return (unsigned char) S_POINTER[s_idx][16 * i + j];
+}
+
+#endif
---
+++ main.cu
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <strings.h>
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include "des_cpu.cuh"
+#include "des_gpu.cu"
+
+#define ERR(source) (perror(source), fprintf(stderr,"%s:%d\n",__FILE__,__LINE__), exit(EXIT_FAILURE))
+
+int main(int argc, char ** argv) {
+
+    uint64 data = 0x0123456789ABCDEF;
+    uint64 key_range_mask = (1ULL << 30) - 1;
+
+    if(argc != 2) {
+        printf("Usage: %s <key_size>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    int key_size = atoi(argv[1]);
+    if(key_size > 64) {
+        printf("Key size reduced to 64 bits.");
+        key_size = 64;
+    }
+    uint64 key = generate_key(key_size, key_range_mask);
+    uint64 encrypted_message = encrypt_message(data, key);
+    clock_t start, end;
+    float time_elapsed;
+
+    // --------- GPU ------------
+
+    int * has_key = NULL;
+    int temp = 0;
+    uint64 * cracked_key = NULL;
+    uint64 found_key;
+    uint64 * d_data = NULL;
+    uint64 * d_msg = NULL;
+
+    cudaError_t error;
+
+    if((error = cudaMalloc((void **) &has_key, sizeof(int))) != cudaSuccess) {
+        ERR(cudaGetErrorString(error));
+    }
+
+    if((error = cudaMalloc((void **) &cracked_key, sizeof(uint64))) != cudaSuccess) {
+        ERR(cudaGetErrorString(error));
+    }
+
+    if((error = cudaMemcpy(has_key, &temp, sizeof(int), cudaMemcpyHostToDevice)) != cudaSuccess) {
+        ERR(cudaGetErrorString(error));
+    }
+    
+    if((error = cudaMalloc((void **) &d_data, sizeof(uint64))) != cudaSuccess) {
+        ERR(cudaGetErrorString(error));
+    }
+    
+    if((error = cudaMalloc((void **) &d_msg, sizeof(uint64))) != cudaSuccess) {
+        ERR(cudaGetErrorString(error));
+    }
+    
+    if((error = cudaMemcpy(d_msg, &encrypted_message, sizeof(uint64), cudaMemcpyHostToDevice)) != cudaSuccess) {
+        ERR(cudaGetErrorString(error));
+    }
+    
+    if((error = cudaMemcpy(d_data, &data, sizeof(uint64), cudaMemcpyHostToDevice)) != cudaSuccess) {
+        ERR(cudaGetErrorString(error));
+    }
+
+    printf("\nGPU : Brute forcing DES...\n");
+    start = clock();
+
+    brute_force<<<256, 128>>>(d_data, d_msg, cracked_key, has_key, key_range_mask);
+
+    if((error = cudaDeviceSynchronize()) != cudaSuccess) ERR(cudaGetErrorString(error));
+    
+    end = clock();
+    time_elapsed = ((float) (end - start)) / CLOCKS_PER_SEC;
+    
+    if((error = cudaMemcpy(&found_key, cracked_key, sizeof(uint64), cudaMemcpyDeviceToHost)) != cudaSuccess) {
+        ERR(cudaGetErrorString(error));
+    }
+
+    printf("GPU : Key found!\n");
+    printf("GPU : Time elapsed - %f\n", time_elapsed);
+    printf("GPU : Cracked key: %llX\n", found_key);
+
+    cudaFree(has_key);
+    cudaFree(cracked_key);
+    cudaFree(d_data);
+    cudaFree(d_msg);
+
+    // --------- CPU -------------
+
+    printf("CPU : Brute forcing DES...\n");
+    
+    start = clock();
+
+    for(uint64 i=0; i<=key_range_mask; i++) {
+        uint64 msg = encrypt_message(data, i);
+        //printBits(i);
+        if(msg == encrypted_message) {
+            end = clock();
+            time_elapsed = ((float) (end - start)) / CLOCKS_PER_SEC;
+            printf("CPU : Key found!\n");
+            printf("CPU : Found key: %llX\n", i);
+            printf("CPU : Time elapsed - %f\n", time_elapsed);
+            break;
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
